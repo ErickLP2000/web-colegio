ca81a5a62697b59e08f31de5d990f892f98e3197
Erick Eduardo Lope Ccollqque <erick.2000.lope@gmail.com>
2024-11-17 19:40:24 -0500

    feat :zap: creation of the teaching profile with visual adjustments

--- profesor/includes/header.php
+++ profesor/includes/header.php
@@ -1,3 +1,9 @@
+<?php
+    session_start();
+    if(empty($_SESSION['activeP'])){
+        header('Location: ./');
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -7,26 +13,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Main CSS-->
-    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/styleprof.css">
+    <link rel="stylesheet" type="text/css" href="../css/main.min.css">
     <!-- Font-icon css-->
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" type="text/css" href="../css/datatables.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/sweetalert2.min.css">
+    <script src="../js/plugins/sweetalert2.min.js"></script>
   </head>
   <body class="app sidebar-mini">
     <!-- Navbar-->
-    <header class="app-header"><a class="app-header__logo" href="index.html">Vali</a>
-      <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
+    <header class="app-header"><a class="app-header__logo" href="index.php">Virgen de Guadalupe</a>
+      <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"><i class="fas fa-bars fa-lg"></i></a>
       <!-- Navbar Right Menu-->
       <ul class="app-nav">
         <li class="app-search">
-          <input class="app-search__input" type="search" placeholder="Search">
-          <button class="app-search__button"><i class="bi bi-search"></i></button>
+          <input class="app-search__input" type="search" placeholder="Buscar">
+          <button class="app-search__button"><i class="fa-solid fa-magnifying-glass"></i></button>
         </li>
         <!-- User Menu-->
-        <li class="dropdown"><a class="app-nav__item" href="#" data-bs-toggle="dropdown" aria-label="Open Profile Menu"><i class="bi bi-person fs-4"></i></a>
+        <li class="dropdown"><a class="app-nav__item" href="#" data-bs-toggle="dropdown" aria-label="Open Profile Menu"><i class="fas fa-user fa-lg"></i></a>
           <ul class="dropdown-menu settings-menu dropdown-menu-right">
-            <li><a class="dropdown-item" href="page-user.html"><i class="bi bi-gear me-2 fs-5"></i> Settings</a></li>
-            <li><a class="dropdown-item" href="page-user.html"><i class="bi bi-person me-2 fs-5"></i> Profile</a></li>
-            <li><a class="dropdown-item" href="page-login.html"><i class="bi bi-box-arrow-right me-2 fs-5"></i> Logout</a></li>
+            <li><a class="dropdown-item" href="page-user.html"><i class="bi bi-gear me-2 fs-5"></i> Configuración</a></li>
+            <li><a class="dropdown-item" href="page-user.html"><i class="bi bi-person me-2 fs-5"></i> Perfil</a></li>
+            <li><a class="dropdown-item" href="../logout.php"><i class="bi bi-box-arrow-right me-2 fs-5"></i> Logout</a></li>
           </ul>
         </li>
       </ul>
